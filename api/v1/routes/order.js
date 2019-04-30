53b5ad14324e023637636e7a719beac28a875635
selvanathan <selvanathan@happyfox.com>
2019-05-01 02:44:39 +0530

    i added short description to category removed id

--- api/v1/routes/order.js
+++ api/v1/routes/order.js
@@ -6,6 +6,7 @@ const orderController = require('./../controllers/order')
 
 app.post('/order', auth.authenticate, orderController.createOrder)
 app.get('/order/:orderId', auth.authenticate, orderController.getOrderById)
+app.get('/order/user/', auth.authenticate, orderController.getOrderByUserId)
 app.get('/order/user/:userId', auth.authenticate, orderController.getOrderByUserId)
 // app.put('/order/:orderId', auth.authenticate, orderController.updateOrderById)
 // app.delete('/order/:orderId', auth.authenticate, orderController.deleteOrderById)
