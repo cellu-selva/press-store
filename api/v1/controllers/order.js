8cead63b164d56d590065f656aa0ece99d040d3b
sarath <sarath.kumar2@tatacommunications.com>
2019-11-18 16:53:27 +0530

    Mail template added for user

--- api/v1/controllers/order.js
+++ api/v1/controllers/order.js
@@ -5,10 +5,15 @@ const objectId = mongoose.Types.ObjectId
 const _ = require('lodash')
 const __ = require('../../../helpers/response')
 const OrderModel = require('../../../models/order')
+const ProductModel = require('../../../models/product')
+const AddressModel = require('../../../models/address')
 const CartModel = require('../../../models/cart')
 const config = require('config')
 const queue = require('./../../../helpers/queue')
 const util = require('./../../../helpers/util')
+const handlebars = require('handlebars');
+const fs = require('fs');
+
 const checkIfOrdersAreValid = function(cartIDs) {
   let error = false
   _.each(cartIDs, (cartId)=> {
@@ -35,6 +40,20 @@ const validateOrder = (data) => {
   return
 }
 
+var readHTMLFile = function (path, callback) {
+  fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+    if (err) {
+      throw err;
+      callback(err);
+    }
+    else {
+      callback(null, html);
+    }
+  });
+};
+
+
+
 const calculatePrice = function(items) {
   let totalPrice = 0
   _.each(items, (item) => {
@@ -52,39 +71,80 @@ const sendAlertMailForOrders = function(order, mailOption) {
   })
 }
 class Order {
+  async productInfo(cartIdems) {
+    
+  }
+
   async createOrder(req, res, next) {
     try {
+      let address = '';
+      let listOfProducts = [];
       const { body, user } = req
       const { orderObj } = body
       validateOrder(orderObj)
-      await CartModel.update({ _id: {
-        $in : orderObj.cartIds
-      }, isDeleted: false }, {
-        $set: {
-          isBilled: true
-        }
-      }, {
-        multi: true
-      })
-      const cartItems = await CartModel.find({ _id: {
-        $in : orderObj.cartIds
-      }, isDeleted: false })
+      await CartModel.update({ _id: { $in : orderObj.cartIds }, isDeleted: false }, { $set: { isBilled: true} }, { multi: true })
+      const cartItems = await CartModel.find({ _id: { $in : orderObj.cartIds }, isDeleted: false })
       if(!cartItems.length) {
         return __.send(res, 400, 'cart Items not found')
+      } else {
+        address = await AddressModel.findOne({ _id: orderObj.address, isDeleted: false });
+
+        for (let i = 0; i < cartItems.length; i++) {
+          let temp = await ProductModel.findOne({ _id: { $in: cartItems[i].product }, isDeleted: false });
+          listOfProducts.push(temp);
+        }
       }
       orderObj.totalPrice = calculatePrice(cartItems)
       let order = new OrderModel(orderObj)
       order.user = user._id
       order.isDeleted = false
       order = await order.save()
-      let mailOptions = {
-        to: user.email,
-        subject: `Order placed - #${order._id}`,
-        html: 'Hi, You\'re order has been placed successfully <br>. Click on the link below to check your order.<br/><br/><br/><br/> '
-      }
-      mailOptions.html += "http://"+ config.get('host') + ":" + config.get('clientPort') +"/my-orders"
-      queue.createJob('sendMail', mailOptions)
-      sendAlertMailForOrders(order, mailOptions);
+
+       readHTMLFile('./templates/order.html', function (err, html) {
+        handlebars.registerHelper('PRODUCTINFO', function (data) {
+          var str = '';
+          for (var i = 0; i < cartItems.length; i++) {
+            str += '<tr>';
+            str += `<td>${i + 1}</td><td>${listOfProducts[i]['name']}</td><td>${cartItems[i].quantity}</td><td>${listOfProducts[i]['discountPrice']}</td>`;
+            str += '</tr>';
+          };
+          str += '';
+
+          return new handlebars.SafeString(str);
+        });
+        
+        var template = handlebars.compile(html);
+        var replacements = {
+          USERNAME: user.firstname,
+          // PRODUCTINFO: productContent,
+          ORDERID: order._id,
+          ADDRESSLINE1: address['addressLine1'],
+          ADDRESSLINE2: address['addressLine2'],
+          CITY: address['city'],
+          STATE: address['state'],
+          PHONE: address['phone'],
+          PINCODE: address['pincode'],
+          BILLINGNAME: address['name'],
+          TOTAL: '₹ ' + (orderObj.totalPrice / 100),
+          DASHBOARD: "http://" + config.get('host') + ":" + config.get('clientPort') + "/my-orders"
+        };
+        var htmlToSend = template(replacements);
+        let mailOptions = {
+          to: user.email,
+          subject: `Pressato - Order placed - #${order._id}`,
+          html: htmlToSend
+        }
+        queue.createJob('sendMail', mailOptions)
+        sendAlertMailForOrders(order, mailOptions);
+      });
+      // let mailOptions = {
+      //   to: user.email,
+      //   subject: `Order placed - #${order._id}`,
+      //   html: 'Hi, You\'re order has been placed successfully <br>. Click on the link below to check your order.<br/><br/><br/><br/> '
+      // }
+      // mailOptions.html += "http://"+ config.get('host') + ":" + config.get('clientPort') +"/my-orders"
+      // queue.createJob('sendMail', mailOptions)
+      // sendAlertMailForOrders(order, mailOptions);
       /*****For admin end */
       let adminMailOptions = {
         to: 'Subs@pressato.in',
@@ -150,7 +210,9 @@ class Order {
       __.error(res, error)
     }
   }
+  
 }
+
 
 
 const obj = new Order()
